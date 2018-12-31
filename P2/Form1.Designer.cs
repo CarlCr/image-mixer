dff42bc6f3f93d1d5ae64104b71a53030c4037fa
Carlos Garcia <gtxcarl9@gmail.com>
2018-12-31 18:16:20 +0100

    Upload Files

---
+++ P2/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace P2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.pic7 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic9 = new System.Windows.Forms.PictureBox();
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic1.Location = new System.Drawing.Point(12, 26);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(100, 100);
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic1_Click);
+            // 
+            // pic2
+            // 
+            this.pic2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Location = new System.Drawing.Point(118, 26);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(100, 100);
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pic2_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic4.Location = new System.Drawing.Point(12, 132);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(100, 100);
+            this.pic4.TabIndex = 3;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.pic4_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic3.Location = new System.Drawing.Point(224, 26);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(100, 100);
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pic3_Click);
+            // 
+            // pic6
+            // 
+            this.pic6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic6.Location = new System.Drawing.Point(224, 132);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(100, 100);
+            this.pic6.TabIndex = 4;
+            this.pic6.TabStop = false;
+            this.pic6.Click += new System.EventHandler(this.pic6_Click);
+            // 
+            // pic7
+            // 
+            this.pic7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic7.Location = new System.Drawing.Point(12, 238);
+            this.pic7.Name = "pic7";
+            this.pic7.Size = new System.Drawing.Size(100, 100);
+            this.pic7.TabIndex = 5;
+            this.pic7.TabStop = false;
+            this.pic7.Click += new System.EventHandler(this.pic7_Click);
+            // 
+            // pic5
+            // 
+            this.pic5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic5.Location = new System.Drawing.Point(118, 132);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(100, 100);
+            this.pic5.TabIndex = 8;
+            this.pic5.TabStop = false;
+            this.pic5.Click += new System.EventHandler(this.pic5_Click);
+            // 
+            // pic9
+            // 
+            this.pic9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic9.Location = new System.Drawing.Point(224, 238);
+            this.pic9.Name = "pic9";
+            this.pic9.Size = new System.Drawing.Size(100, 100);
+            this.pic9.TabIndex = 7;
+            this.pic9.TabStop = false;
+            this.pic9.Click += new System.EventHandler(this.pic9_Click);
+            // 
+            // pic8
+            // 
+            this.pic8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic8.Location = new System.Drawing.Point(118, 238);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(100, 100);
+            this.pic8.TabIndex = 6;
+            this.pic8.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(508, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(387, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Selecione uma foto ";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(508, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Baralhar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(713, 358);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic9);
+            this.Controls.Add(this.pic8);
+            this.Controls.Add(this.pic7);
+            this.Controls.Add(this.pic6);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.PictureBox pic7;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic9;
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ P2/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace P2
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> listaPic ;
+        public Image novafoto;
+        public Form1()
+        {
+          
+         
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listaPic = new List<PictureBox>()
+            {
+                pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8,pic9
+            };
+        }
+        public void distribuitFoto()
+        {
+            Bitmap foto;
+            Graphics grafico;
+            int pos1=0;
+            int pos2=0;
+            Random aleatorio = new Random();
+            int index = 0;
+            foreach (PictureBox pic in listaPic)
+            {
+                index = aleatorio.Next(0,9);
+                foto = new Bitmap(100, 100);
+                grafico = Graphics.FromImage(foto);
+                grafico.DrawImage(novafoto,
+                    new Rectangle(0, 0, 100, 100),
+                    new Rectangle(pos1, pos2, 100, 100),
+                    GraphicsUnit.Pixel);
+                listaPic[index].Image = foto;
+                pos1 += 100;
+                if (pos1 == 300)
+                {
+                    pos1 = 0;
+                    pos2 += 100;
+                }
+                
+            }
+ 
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            Bitmap  endereco = new Bitmap(openFileDialog1.FileName);
+            Bitmap foto = new Bitmap(endereco, pictureBox1.Size);
+            pictureBox1.Image = foto;
+            novafoto = foto;
+            distribuitFoto();
+             
+        }
+        public void Randon()
+        {
+            
+        }
+
+        public void mover(PictureBox p2,PictureBox p1)
+        {
+            if (p1.Image==pic9.Image)
+            {
+                p1.BackgroundImage = p2.BackgroundImage;
+                p2.BackgroundImage = pic9.BackgroundImage;
+            }
+        }
+
+        private void pic2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pic7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            distribuitFoto();
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
